6ad8615ee120ca179ea727341b47c31c3a58fc2a
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-08-07 20:37:30 +0400

    реализует рендер 2 и 3 страниц

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import { cards } from "./components/cards-component.js"
+
 const appElement = document.getElementById("app")
 let globalState = {}
-const renderChooseFormPage = (element) => {
+const renderApp = (element,  cartSide) => {
     const chooseFormHtml = `<form class="choose-form">
     <h1 class="title">Выбери <br> сложность</h1>
     <div class="complexity-radio-box">
@@ -12,7 +14,7 @@ const renderChooseFormPage = (element) => {
         <label for="radio3">3</label>
     </div>
     <div class="start-submit-box">
-        <button type="submit" class="start-button">Старт</button>
+        <button type="submit" class="button">Старт</button>
     </div>
     </form>`
     element.innerHTML = chooseFormHtml
@@ -38,14 +40,56 @@ const renderChooseFormPage = (element) => {
                 complexity: complexityNumber,
             }
             console.log(globalState.complexity)
-            renderPlayingField(element, globalState.complexity)
+            renderPlayingField(element, globalState.complexity,cartSide)
         }
     })
 }
-renderChooseFormPage(appElement)
+renderApp(appElement)
+
 const renderPlayingField = (element, number) => {
+    const cardsHtml = cards.map((card) =>{
+        if (card.isClicked == true) {
+            return card.backImg
+        } else {
+            return`<div class="card item">
+        <div class="rank header-rank">
+            <div class="rank-box">
+                <p class="rank-text">${card.rank}</p>
+                ${card.suitImgLittle}  
+            </div>
+        </div>
+        <div class="suit"> 
+            ${card.suitImgBig}        
+        </div>
+        <div class="rank footer-rank">
+            <div class="rank-box">
+                <p class="rank-text footer-rank-text">${card.rank}</p>
+                ${card.suitImgLittle} 
+            </div>
+        </div>
+        </div>`
+        }
+    }).join('');
     const playingFieldHtml = `
-    <h2>Выбран уровень сложности ${number}</h2>
-    <div>Здесь будет игровое поле</div>`
+    <!--<h2>Выбран уровень сложности ${number}</h2>-->
+    <div class="header">
+        <div class="timer">
+            <div class="units">
+                <p class="unit-item">min</p>
+                <p class="unit-item">sek</p>
+            </div>
+            <div class="time"><p>00.00</p></div>
+        </div>
+        <button class="button">Начать заново</button>
+    </div>
+    <div class="playing-field">
+    ${cardsHtml}
+    </div>`
     element.innerHTML = playingFieldHtml
 }
+
+
+
+
+
+
